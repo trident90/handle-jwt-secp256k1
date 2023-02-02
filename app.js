2b1559c30a3e57626a12432a6f6cbb5e7b23879e
Jongsik Jung <jongsik.jung@coinplug.com>
2023-02-02 16:51:00 +0900

    submit interim version.

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const ecKeyUtils = require('eckey-utils');
 const jose = require('jose');
 const wallet = require('ethereumjs-wallet').default;
 const fs = require('fs');
+const Web3 = require('web3');
+var KeyEncoder = require('@tradle/key-encoder').default, keyEncoder = new KeyEncoder('secp256k1');
+
 
 const alg = 'ES256K'
 const curveName = 'secp256k1'
 
 const keystoreFile = '/Users/trident/Temp/keystore/account-1'
+//const keystoreFile = '/Users/trident/Temp/keystore/account-jwt'
 const passwd = 'demo'
 
 const _getKeyPair = async () => {
@@ -15,7 +19,7 @@ const _getKeyPair = async () => {
     generateKeyPair('ec', {
       namedCurve: 'secp256k1',
       publicKeyEncoding: { type: 'spki', format: 'pem' },
-      privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
+      //privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
     }, (err, publicKey, privateKey) => {
       if (err) return reject(err);
       resolve({publicKey, privateKey});
@@ -24,52 +28,48 @@ const _getKeyPair = async () => {
 };
 
 async function main() {
-  const myWallet = await wallet.fromV3(fs.readFileSync(keystoreFile).toString(), passwd, true);
-  const ethPrivKey = myWallet.getPrivateKeyString();
-  const ethPubKey = myWallet.getPublicKeyString();
-  console.log('PrivateKey: ', ethPrivKey.substring(2));
-  console.log('Address: ', myWallet.getAddressString());
-  const ePrivKey = Buffer.from(ethPrivKey.substring(2), 'hex');
-  const ePubKey = Buffer.from(ethPubKey.substring(2), 'hex');
-  const pems = ecKeyUtils.generatePem({
-    curveName,
-    privateKey: ePrivKey,
-    publicKey: ePubKey,
-  });
-  console.log('pems: ', pems);
+  let privKeyPEM = null;
+  let pubKeyPEM = null;
 
-  /*
-  const aaa = createECDH('secp256k1');
-  aaa.setPrivateKey(ePrivKey);
-  let keyPair = await crypto.subtle.importKey(
-    'raw',
-    ePrivKey,
-    { name: 'ECDSA' },
-    true,
-    ['sign', 'verify']
-  );
-  //console.log('PEM:', await aaa.
-  console.log('getPrivKey: ', aaa.getPrivateKey().toString('hex'));
-  console.log('getPubKey: ', aaa.getPublicKey().toString('hex'));
-  */
-  /*
-  const keyPair = await _getKeyPair();
-  console.log('publicKey: ', keyPair.publicKey.toString());
-  console.log('PrivateKey: ', keyPair.privateKey.toString());
-  const key = `${keyPair.privateKey.toString()}`;
-  //const privKey = await jose.importPKCS8(keyPair.privateKey.toString, alg);
-  */
-  //console.log('key: ', ecKeyUtils.parsePem(pems).privateKey);
-  //const privKey = await jose.importPKCS8(`${pems.privateKey}`, alg);
+  if (fs.existsSync(keystoreFile)) {
+    const myWallet = await wallet.fromV3(fs.readFileSync(keystoreFile).toString(), passwd, true);
+    const ethPrivKey = myWallet.getPrivateKeyString();
+    const ethPubKey = myWallet.getPublicKeyString();
+    console.log('PrivateKey: ', ethPrivKey.substring(2));
+    console.log('Address: ', myWallet.getAddressString());
+    const ePrivKey = Buffer.from(ethPrivKey.substring(2), 'hex');
+    const ePubKey = Buffer.from(ethPubKey.substring(2), 'hex');
+    //let key = await keyEncoder.encodePrivate(keyPair.privateKey.export({type: 'sec1', format: 'pem'}), 'pem', 'raw');
+    privKeyPEM = await keyEncoder.encodePrivate(ethPrivKey.substring(2), 'raw', 'pem', 'pkcs8');
+    pubKeyPEM = await keyEncoder.encodePublic(ethPubKey.substring(2), 'raw', 'pem');
+    console.log(pubKeyPEM);
+  } else  {
+    const keyPair = await _getKeyPair();
+    console.log('publicKey: ', keyPair.publicKey.toString());
+    console.log('PrivateKey: ', keyPair.privateKey.export({type:'pkcs8', format:'pem'}));
+
+    let key = await keyEncoder.encodePrivate(keyPair.privateKey.export({type: 'sec1', format: 'pem'}), 'pem', 'raw');
+    console.log('typeof: ', typeof key, ', key: ', key);
+    const ePrivKey = Buffer.from(key, 'hex');
+    const myWallet = await wallet.fromPrivateKey(ePrivKey);
+    const keystore = await myWallet.toV3String('demo');
+    console.log('keystore: ', keystore);
+//    fs.writeFileSync(keystoreFile, keystore);
+    privKeyPEM = keyPair.privateKey.export({type: 'pkcs8', format: 'pem'});
+    console.log(keyPair.privateKey.type)
+    pubKeyPEM = keyPair.publicKey.toString();
+  }
+  const privKey = await jose.importPKCS8(privKeyPEM, alg);
+
   const jwt = await new jose.SignJWT( {'urn:example:clami': true })
     .setProtectedHeader({ alg })
     .setIssuedAt()
     .setIssuer('urn:example:issuer')
     .setAudience('urn:example:audience')
     .sign(privKey);
-  console.log(jwt);
+  console.log('jwt: ', jwt);
 
-  const pubKey = await jose.importSPKI(`${keyPair.publicKey.toString()}`, alg);
+  const pubKey = await jose.importSPKI(pubKeyPEM, alg);
 //  const { payload, protectedHeader } = await jose.jwtVerify(jwt, pubKey, {
 //    issuer: 'urn:example:issuer',
 //    audience: 'urn:example:audience',
